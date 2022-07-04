6ce7b0f662a74ee2bc4448799932e27919db86e9
Sameer Alam <sameeralamynwa@gmail.com>
2022-07-05 01:07:20 +0530

    Improved responsiveness

---
+++ static/loginsuccess.js
@@ -0,0 +1,9 @@
+/*
+setTimeout(() => {
+    document.getElementsByClassName('register')[0].getElementsByTagName('li')[0].style.display = 'none';
+    document.getElementsByClassName('register')[0].getElementsByTagName('li')[1].style.display = 'none';
+
+    document.getElementsByClassName('logged')[0].getElementsByTagName('li')[0].getElementsByTagName('a')[0].innerText = document.getElementsByClassName('user')[0].innerText;
+    document.getElementsByClassName('logged')[0].getElementsByTagName('li')[0].style.display = 'block';
+}, 0);
+*/
