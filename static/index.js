198d12bc145d0c5fcb6d82201a8c47dde0960ee0
Sameer Alam <sameeralamynwa@gmail.com>
2022-08-03 23:17:16 +0530

    Modified Topic Schema

--- static/index.js
+++ static/index.js
@@ -37,3 +37,30 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     copyButton.addEventListener('click', copyTextHandler);
   });
+
+
+let java = document.getElementById('java');
+
+java.addEventListener('click', function (){
+  let content = document.getElementById('code');
+  console.log(content)
+  content.style.display = 'none';
+  alert('We are working');
+})
+
+let python = document.getElementById('python');
+
+python.addEventListener('click', function (){
+  let content = document.getElementById('code');
+  console.log(content)
+  content.style.display = 'none';
+  alert('We are working');
+})
+
+let cpp = document.getElementById('cpp');
+
+cpp.addEventListener('click', function (){
+  let content = document.getElementById('code');
+  console.log(content)
+  content.style.display = 'block';
+})
